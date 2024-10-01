9ad9dea27c2e64fe96c6754754ba9f0766bc4624
Luis <ramirez21ler@gmail.com>
2024-10-01 17:25:54 -0400

    pushing restful changes

--- web_app_python/main.py
+++ web_app_python/main.py
@@ -11,6 +11,16 @@ def hello_world():
     """Example Hello World route."""
 
     return f"Hello World!!!!!!"
+
+@app.route("/user", methods=['GET', 'POST'])
+def user():
+    """Example user route."""
+    # Get the user name from the request
+    user_name = request.args.get('name')
+    # Return the user name
+    return f"Hello {user_name}!"
+
+    # return f"User Page"
     
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
